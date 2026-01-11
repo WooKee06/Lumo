ca6bb5bf29531481f45caf0d9a24b6d80c5134af
WooKee06 <suleimanovkamil8@gmail.com>
2026-01-11 22:16:10 +0300

    test commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/shared/Header/ui/Header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1>Lumo main page</h1>
+    <Header/>
+    </>
+  );
+}
---
+++ shared/Profile/ui/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import s from './Profile.module.scss'
+
+const Profile = () => {
+  return (
+    <div>
+      Profile
+    </div>
+  )
+}
+
+export default Profile
