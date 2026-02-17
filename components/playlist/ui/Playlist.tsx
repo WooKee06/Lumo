ecd1a8621204eb578f729e42f231f62b4945905e
WooKee06 <suleimanovkamil8@gmail.com>
2026-02-17 10:05:49 +0300

    update playlist component

--- components/playlist/ui/Playlist.tsx
+++ components/playlist/ui/Playlist.tsx
@@ -3,14 +3,11 @@
 import s from './Playlist.module.scss';
 
 import { useEffect } from 'react';
-import { GetMusic } from '../api/MusicApi';
 import { playerStore } from '../store/TrackStore';
 
 import Track from '@/shared/ui/Track/Track';
 import { observer } from 'mobx-react-lite';
 import { useDebouce } from '@/shared/hooks/useDebounce';
-import { useSeach } from '@/shared/hooks/useSearch';
-import { CiLight } from 'react-icons/ci';
 
 const Playlist = observer(() => {
   const debouncedValue = useDebouce(playerStore.searchValue, 300);
